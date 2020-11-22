ca5013c8ee7de880e469d70da94ebc53b73b526a
Khazise <71380558+Khazise@users.noreply.github.com>
2020-11-22 23:31:34 +0100

    commit

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const net = require('net')
 const fs = require('fs')
-const readline = require('readline');
-const { dirname } = require('path');
+const readline = require('readline')
+const path = require('path')
 
 const port = process.argv[2]
 const data = fs.readFileSync('user.json');
 const user = JSON.parse(data);
 
-let username = ''
 
 const server = net.createServer((socket) => {
   console.log('new connection')
-
+  let username = ''
+  let connected = false;
   socket.on('data', (data) => {
     const [directive, parameter] = data.toString().split(' ')
 
@@ -39,22 +39,57 @@ const server = net.createServer((socket) => {
                 if(data.username == username & data.password == parameter)
                 {
                     pwdMessage = 'You are connected'
+                    connected = true;
                 }    
               });
             socket.write(pwdMessage)
             break;
         
-        case 'LIST':
+        case 'PWD':
+          if(connected == false) break;
             socket.write(`Current directory of the server : ${process.cwd()}`)
             break;
         
         case 'CWD': 
+        if(connected == false) break;
         try {
             process.chdir(parameter);
             console.log(`New directory: ${process.cwd()}`);
           } catch (err) {
             console.error(`chdir: ${err}`);
           }
+          break;
+
+        case 'LIST' : 
+        if(connected == false) break;
+        socket.write("List :")
+        fs.readdir(process.cwd(), (err, items) => {
+
+          for (var i=0; i<items.length; i++) {
+            socket.write(items[i] + '\n');
+          }
+        });
+        break;
+            
+        /*case 'RETR':
+          const writeStream = fs.createWriteStream(parameter)
+          socket.write(writeStream.toString())
+          break; 
+        */ 
+
+        case 'QUIT' :
+          if(connected == false) break; 
+        socket.end()
+
+        case 'HELP' :
+        let help = "USER <username>: check if the user exist PASS <password>: authenticate the user with a password \n"+
+                    "LIST: list the current directory of the server\n"+
+                    "CWD <directory>: change the current directory of the server\n"+
+                    "RETR <filename>: transfer a copy of the file FILE from the server to the client\n" +
+                    "STOR <filename>: transfer a copy of the file FILE from the client to the server\n"+
+                    "PWD: display the name of the current directory of the server \n"+
+                    "QUIT: close the connection and stop the program";
+        socket.write(help)
 
     }
   })
